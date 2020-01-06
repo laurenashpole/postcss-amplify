42f237fb6a7cc91aec08ecb1677948014ead5706
Lauren Ashpole <lauren@laurenashpole.com>
2020-01-06 13:31:52 -0500

    Initial commit

--- packages/postcss-remove-amp-styling/index.js
+++
@@ -1,9 +0,0 @@
-const postcss = require('postcss')
-
-module.exports = postcss.plugin('postcss-remove-amp-styling', () => {
-  return root => {
-    root.walkRules(/^(\.-amp|-i-amp)/, rule => {
-      rule.remove()
-    })
-  }
-})
--- packages/postcss-unimportant/index.js
+++
@@ -1,9 +0,0 @@
-const postcss = require('postcss')
-
-module.exports = postcss.plugin('postcss-unimportant', () => {
-  return root => {
-    root.walkDecls(decl => {
-      decl.important = false
-    })
-  }
-})
