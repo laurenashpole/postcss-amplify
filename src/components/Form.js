d00670a49128508cfcd969330fee86b374189e3a
Lauren Ashpole <lauren@laurenashpole.com>
2020-01-13 14:31:08 -0500

    Added info tooltips

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import './Form.css';
 import postcss from 'postcss';
 import postcssAmplify from 'postcss-amplify';
+import Tooltip from 'react-tooltip';
 import { DEFAULT_INPUT } from '../constants/defaultInput';
 
 const Form = () => {
@@ -61,14 +62,16 @@ const Form = () => {
           <h4>Options</h4>
 
           <div className="form__opts-group">
-            <label htmlFor="maxBreakpoint">Max breakpoint:</label>
+            <label htmlFor="maxBreakpoint">Max breakpoint: <button className="form__tooltip-btn" data-tip data-for="maxBreakpointTooltip" onClick={(e) => e.preventDefault()}><span className="form__tooltip-icon">i</span></button></label>
             <input className="form__opts-input" id="maxBreakpoint" type="text" value={maxBreakpoint} onChange={(e) => setMaxBreakpoint(e.target.value)} />
+            <Tooltip className="form__tooltip" id="maxBreakpointTooltip" place="bottom" type="light" effect="solid" aria-haspopup="true">Media queries below this breakpoint will be preserved</Tooltip>
           </div>
 
           <div className="form__opts-group">
-            <label htmlFor="blockText">Blocks/prefixes to exclude:</label>
+            <label htmlFor="blockText">Blocks/prefixes to exclude: <button className="form__tooltip-btn" data-tip data-for="blocksTooltip" onClick={(e) => e.preventDefault()}><span className="form__tooltip-icon">i</span></button></label>
             <input className="form__opts-input" id="blockText" type="text" value={blockText} onChange={(e) => setBlockText(e.target.value)} />
             <button className="form__opts-btn" onClick={handleAddBlock}>Add</button>
+            <Tooltip className="form__tooltip" id="blocksTooltip" place="bottom" type="light" effect="solid" aria-haspopup="true">Class blocks or prefixes to exclude (especially useful with BEM or other namespacing methodologies)</Tooltip>
           </div>
 
           <ul className="form__opts-blocks">
