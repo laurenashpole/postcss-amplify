ae13b9abf2aef9352234fb4cd302d4428edff864
Lauren Ashpole <lauren@laurenashpole.com>
2020-01-13 17:07:51 -0500

    Updates

---
+++ precache-manifest.7749c4ccef0ac121e74a355e7d0737cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfacd00a6a3fd32b6025de30452e14c3",
+    "url": "/postcss-amplify/index.html"
+  },
+  {
+    "revision": "87a7014c0458d632a148",
+    "url": "/postcss-amplify/static/css/main.bd586eb7.chunk.css"
+  },
+  {
+    "revision": "578134262e36e4ec0ef6",
+    "url": "/postcss-amplify/static/js/2.ec55af7b.chunk.js"
+  },
+  {
+    "revision": "7e226df6a878c6689b6fee853b389a93",
+    "url": "/postcss-amplify/static/js/2.ec55af7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87a7014c0458d632a148",
+    "url": "/postcss-amplify/static/js/main.77464ae2.chunk.js"
+  },
+  {
+    "revision": "4c0c60eaeaf0f542015e",
+    "url": "/postcss-amplify/static/js/runtime-main.fe63992d.js"
+  }
+]);
